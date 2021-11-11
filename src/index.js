8a0a8f7cd4ac76deb36abe83f32fb0126fa1dfe1
Martha Kerr <mjlkerr77@gmail.com>
2021-11-11 09:23:29 -0500

    adding redux thunk, reorg calculating dd values

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
-import {createStore} from 'redux';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
 import rootReducer from './rootReducer';
+import thunk from 'redux-thunk';
 
 export default () => {
-  return createStore(rootReducer);
+  return createStore(rootReducer, applyMiddleware(thunk));
 };
